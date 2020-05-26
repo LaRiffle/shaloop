15baa2a351717eb2f71c09fe43bc90356acf92d0
Théo Ryffel <theo.leffyr@gmail.com>
2020-05-26 15:03:45 +0200

    Add v0 implementations

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup, Extension
+import numpy
+from Cython.Distutils import build_ext
+
+setup(
+    cmdclass={"build_ext": build_ext},
+    ext_modules=[
+        Extension(
+            "sha_loop",
+            sources=["src/_sha_loop.pyx", "src/sha_loop.c"],
+            include_dirs=[numpy.get_include()],
+        )
+    ],
+)
---
+++ src/sha_loop.h
@@ -0,0 +1,2 @@
+void sha256_loop(uint8_t * buffer, int n_values, uint8_t * out);
+void sha512_loop(uint8_t * buffer, int n_values, uint8_t * out);
